b478c301bf1a8ad4914cac6e5692fcf232fd6214
JoshuaGit-Hub <150089591+JoshuaGit-Hub@users.noreply.github.com>
2025-10-18 14:39:04 -0600

    Refactor search and filter UI; add movie data structure
    Updated search input and filter buttons to use class selectors for improved styling and maintainability. Added initial movie data structure and placeholder for movie rendering logic in script.js. Enhanced styles for filter buttons and search section for better UI consistency.

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-const lupa = document.querySelector('#lupa');
-const inputNav = document.querySelector('#input-procurar-nav');
+const lupa = document.querySelector('.lupa');
+const inputNav = document.querySelector('.inputs-busca');
 
 lupa.addEventListener('click', () => {
-    if(!inputNav.classList.contains('clicked')){
+    if (!inputNav.classList.contains('clicked')) {
         inputNav.classList.add('clicked');
         lupa.classList.add('girar');
-    }else{
+    } else {
         inputNav.classList.remove('clicked');
         lupa.classList.remove('girar');
     }
 });
+
+const movies = [{
+        id: 1,
+        title: 'Caramelo',
+        ano: 2025,
+        genre: ['Drama', 'Romance'],
+        favorite: false
+    },
+    {
+        id: 2,
+        title: 'Coco',
+        ano: 2018,
+        genre: ['Drama', 'Comedy'],
+        favorite: false
+    },
+    {
+        id: 3,
+        title: 'It a Coisa',
+        ano: 2017,
+        genre: ['Terror', 'Drama'],
+        favorite: false
+    }
+];
+
+let moviesAtuais = [movies];
+let gridAtual = 'todos';
+let procurado = '';
+
+function mostrarMovies() {
+
+}
